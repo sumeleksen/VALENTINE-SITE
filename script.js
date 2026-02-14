eb2fea5844e1300f961119e806ca99b2133fc84e
Asem <asem@MacBook-Air-Asem.local>
2026-02-14 18:31:42 +0500

    Добавлены страницы 3 и 4 с фото

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function nextPage() {
     document.getElementById('page1').classList.add('hidden');
     document.getElementById('page2').classList.remove('hidden');
+    document.getElementById('page3').classList.add('hidden');
+    document.getElementById('page4').classList.remove('hidden');
 
     const audio = document.getElementById('music');
     audio.play().catch(err => {
